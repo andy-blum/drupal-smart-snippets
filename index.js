da79c98bd65b9280468a4ec684be25b36df47d06
Andy Blum <andy.blum.01@gmail.com>
2023-04-13 21:17:35 -0400

    un-comment del/download code

--- index.js
+++ index.js
@@ -22,21 +22,21 @@ let allElements = {};
 let allServices = {};
 
 // 2. Clean tmp directory.
-// try {
-//   const contents = await readdir('./tmp');
-//   for (const item of contents) {
-//     await rm(`./tmp/${item}`, {
-//       force: true,
-//       recursive: true
-//     });
-//   }
-// } catch (error) {}
+try {
+  const contents = await readdir('./tmp');
+  for (const item of contents) {
+    await rm(`./tmp/${item}`, {
+      force: true,
+      recursive: true
+    });
+  }
+} catch (error) {}
 
 for (const version of SUPPORTED_VERSIONS) {
   console.log(`Drupal ${version}`);
 
   // 3. Download & un-archive tarball.
-  // await getCoreVersion(version);
+  await getCoreVersion(version);
   console.log(`  - Downloaded`);
 
   /*******************************
