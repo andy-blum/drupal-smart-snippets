f12924cb90a32a52c4bbd52ef61c8e75ab664c3e
Andy Blum <andy.blum.01@gmail.com>
2024-06-20 21:46:27 -0400

    add core module snippets

--- src/getElementFiles.js
+++ src/getElementFiles.js
@@ -10,7 +10,7 @@ export async function getElementFiles(version) {
 	return new Promise(async (res) => {
 		const lib = await find.fileSync(`tmp/drupal-${version}/core/lib`);
 		const mod = await find.fileSync(`tmp/drupal-${version}/core/modules`);
-		const files = filterElementFiles([...lib]);
+		const files = filterElementFiles([...lib, ...mod]);
 		res(files);
 	});
 }
