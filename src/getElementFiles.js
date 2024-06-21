63f56ab77cf1c1f76171aab0a55a6f010564cdb9
Andy Blum <andy.blum.01@gmail.com>
2024-06-20 21:41:28 -0400

    update elements scraper

--- src/getElementFiles.js
+++ src/getElementFiles.js
@@ -1,10 +1,16 @@
 import find from "find";
 
+function filterElementFiles(files) {
+	return files.filter((filename) => (
+		filename.match(/\/Element\/[^\.]*\.php/g)
+	));
+}
+
 export async function getElementFiles(version) {
-	return new Promise((res) => {
-		find.file(`tmp/drupal-${version}/core/lib/Drupal/Core/Render/Element/`, (file) => {
-			const filtered = file.filter((filename) => filename.endsWith('.php'));
-			res(filtered);
-		});
+	return new Promise(async (res) => {
+		const lib = await find.fileSync(`tmp/drupal-${version}/core/lib`);
+		const mod = await find.fileSync(`tmp/drupal-${version}/core/modules`);
+		const files = filterElementFiles([...lib]);
+		res(files);
 	});
 }
